c98b1d16749ee5507d8b555930eeb92f556fbca5
Matt Penrice <matthew.penrice@gmail.com>
2018-01-07 07:50:17 +0000

    Includes failing test

--- src/DeprecatedNoticeTrigger.php
+++ src/DeprecatedNoticeTrigger.php
@@ -13,4 +13,18 @@ class DeprecatedNoticeTrigger {
         @trigger_error('Bundle inheritance is deprecated as of 3.4 and will be removed in 4.0.', E_USER_DEPRECATED);
         throw new \RuntimeException("this is expected");
     }
+    
+    public function triggerDeprecatedNoticeWithExceptionAndCustomErrorHandler() {
+        
+        set_error_handler(function($errno, $errstr, $errfile, $errline, array $errcontext) {
+            throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+        });
+        try {
+            @trigger_error('This should be the exception message that is expected', E_USER_WARNING);
+        }
+        catch (\Throwable $e) { // in this case would be our \ErrorException
+            throw new \RuntimeException($e->getMessage());
+        }
+        restore_error_handler();
+    }
 }
--- tests/DeprecatedNoticeTriggerTest.php
+++ tests/DeprecatedNoticeTriggerTest.php
@@ -8,22 +8,38 @@ class DeprecatedNoticeTriggerTest extends TestCase {
 
     public function setUp() {
         $this->subject = new DeprecatedNoticeTrigger;
+
+        // as experienced in https://github.com/symfony/symfony/blob/3.4/src/Symfony/Component/HttpKernel/Kernel.php#L502
+        // and normally thrown as part of the symfony KernelWebTest bootstrapping in setUp()
         @trigger_error('Bundle inheritance is deprecated as of 3.4 and will be removed in 4.0.', E_USER_DEPRECATED);
     }
 
-    // passes as expected
-    public function testTriggerDeprecatedNotice() {
-        $this->assertTrue($this->subject->triggerDeprecatedNotice());
-    }
-    
-    // the expected exception somehow surfaces the deprecated error. Notice
-    // how the phpunit.xml.dist file specfies 
-    // convertDeprecationsToExceptions="false"
-    public function testTriggerDeprecatedNoticeWithExpectedExceptionTest() {
+    public function testTriggerDeprecatedNoticeAlongsideCustomErrorHandler($dummy) {
         
         $this->expectException(\RuntimeException::CLASS);
-        $this->expectExceptionMessage("this is expected");
+        $this->expectExceptionMessage("This should be the exception message that is expected");
         
-        $this->subject->triggerDeprecatedNoticeWithException();
+        $this->subject->triggerDeprecatedNoticeWithExceptionAndCustomErrorHandler();
+    }
+
+    /**
+     * The dataProvider use seems to be the root cause here
+     * 
+     * @dataProvider dataProvider
+     */
+    public function testTriggerDeprecatedNoticeAlongsideCustomErrorHandlerWithDataProvider(String $dummy) {
+        
+        $this->expectException(\RuntimeException::CLASS);
+        $this->expectExceptionMessage("This should be the exception message that is expected");
+        
+        $this->subject->triggerDeprecatedNoticeWithExceptionAndCustomErrorHandler();
+    }
+
+    public function dataProvider() {
+        return [
+            ['dummy'],
+            ['dummy'],
+            ['dummy'],
+        ];
     }
 }
